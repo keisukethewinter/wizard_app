5402e220463c46e8227dadc562eef4915ce4e009
keisukethewinter <addict20fx1@gmail.com>
2020-09-28 23:39:39 +0900

    住所を登録させるページを表示するようにroutes.rbを編集
    住所情報を登録させるページを表示するnew_addressアクションと住所情報を登録するcreate_addressアクションのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+
+  devise_scope :user do
+    get 'addresses', to: 'users/registration#new_address'
+    post 'addresses', to: 'users/registration#create_address'
+  end
+
   root to: "home#index"
 end
