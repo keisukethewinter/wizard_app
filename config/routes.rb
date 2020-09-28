c56b41a83460dfdab88f0e7fa0472d492b697325
keisukethewinter <addict20fx1@gmail.com>
2020-09-28 19:43:10 +0900

    routes.rbに紐づけるコントローラーを明示

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'home/index'
+  devise_for :users, controllers: {
+    registrations: 'user/registrations'
+  }
   root to: "home#index"
 end
