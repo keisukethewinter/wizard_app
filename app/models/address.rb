8ea67b4b8d9affc9513e9de5761cd9ef9c763573
keisukethewinter <addict20fx1@gmail.com>
2020-09-28 19:32:36 +0900

    modelにvalidationとassociationを記述

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,4 @@
 class Address < ApplicationRecord
+  belongs_to :user, optional: true #住所情報を入力した時点で、user_idには値が入っていないので、通常ではバリデーションに引っかかり、DBに保存できません。
+  validates :postal_code, :address, presence: true
 end
