bc486ccdb8e5bfe9582f2393ce542c7276aaf02f
keisukethewinter <addict20fx1@gmail.com>
2020-09-29 01:16:02 +0900

    typoの修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   }
 
   devise_scope :user do
-    get 'addresses', to: 'users/registration#new_address'
-    post 'addresses', to: 'users/registration#create_address'
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
   end
 
   root to: "home#index"
